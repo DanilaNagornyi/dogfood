05c495f2dde14e5ae446680c4b4f626af87b29c6
Nagornyi <artnagornyi@gmail.com>
2023-02-09 17:12:22 +0300

    Выкосил библиотеку ANTD

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
-import AppAnt from "./components/AppAnt/AppAnt";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AppAnt />);
+root.render(<App />);
 
